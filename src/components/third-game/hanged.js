78cb40c00f2a698864040c67846fcaa198d2f2d6
AndreaILara <andreainigolara@hotmail.com>
2024-02-05 09:55:01 +0100

    public

--- src/components/third-game/hanged.js
+++ src/components/third-game/hanged.js
@@ -21,7 +21,7 @@ export function startHangedGame() {
 
   const hangmanImage = document.createElement("img");
   hangmanImage.id = "hangman-image";
-  hangmanImage.src = "public/hangman-0.png";
+  hangmanImage.src = "/hangman-0.png";
 
   const wordContainer = document.createElement("div");
   wordContainer.id = "word-container";
@@ -76,7 +76,7 @@ export function startHangedGame() {
       attemptsContainer.textContent = `Remaining attempts: ${remainingAttempts}`;
 
 
-      hangmanImage.src = `public/hangman-${6 - remainingAttempts}.png`;
+      hangmanImage.src = `/hangman-${6 - remainingAttempts}.png`;
 
 
       if (remainingAttempts === 0) {
